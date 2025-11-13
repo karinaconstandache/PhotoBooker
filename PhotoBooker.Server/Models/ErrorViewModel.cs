5f481ef0b544a23f5f6d2fccb901b85edfd61e8b
Karina-Melissa Constandache <karinaconstandache12@gmail.com>
2025-11-13 22:23:01 +0200

    Added mvc project files

---
+++ PhotoBooker.Server/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PhotoBooker.Server.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
